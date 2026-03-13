5ce330e881b82d40a784df3865dd998ce9958954
shamal35 <shamaltheekshana36@gmail.com>
2026-03-13 11:40:30 +0530

    Fix import style for @vercel/analytics in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx';
 import './index.css';
 
